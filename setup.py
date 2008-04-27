367b1246fc9838e42160d9dc79bafc802cde7c5e
Bob Farrell <bob@keys.(none)>
2008-04-27 22:05:23 +0100

    Whoosh, I've switched over to an installable package.
    Pascal Bleser was kind enough to package bpython for OpenSUSE
    and sent me a patch to have it working with distutils, which
    is always nice, so thanks a lot for that, here's the new
    dir structure.
    
    committer: Bob Farrell <bob@keys.(none)>

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from distutils.command.install_data import install_data
+from distutils.sysconfig import get_python_lib
+from distutils.core import setup, Extension
+from distutils.dep_util import newer
+from distutils.log import info
+from distutils import sysconfig
+import distutils.file_util
+import distutils.dir_util
+import sys, os
+import glob
+import re
+
+# Make distutils copy bpython.py to bpython
+copy_file_orig = distutils.file_util.copy_file
+copy_tree_orig = distutils.dir_util.copy_tree
+def copy_file(src, dst, *args, **kwargs):
+    if dst.endswith("bin/bpython.py"):
+        dst = dst[:-3]
+    return copy_file_orig(src, dst, *args, **kwargs)
+def copy_tree(*args, **kwargs):
+    outputs = copy_tree_orig(*args, **kwargs)
+    for i in range(len(outputs)):
+        if outputs[i].endswith("bin/bpython.py"):
+            outputs[i] = outputs[i][:-3]
+    return outputs
+distutils.file_util.copy_file = copy_file
+distutils.dir_util.copy_tree = copy_tree
+
+PYTHONLIB = os.path.join(get_python_lib(standard_lib=1, prefix=""),
+                         "site-packages")
+
+setup(name="bpython",
+      version = "0.2.4",
+      description = "Fancy Interface to the Python Interpreter",
+      author = "Robert Anthony Farrell",
+      author_email = "robertanthonyfarrell@gmail.com",
+      license = "MIT/X",
+		url = "http://www.noiseforfree.com/bpython/",
+      long_description =
+"""\
+bpython is a fancy interface to the Python interpreter for Unix-like operating systems.
+""",
+      packages = ["bpython"],
+      scripts = ["bpython.py"],
+      )
+
+
