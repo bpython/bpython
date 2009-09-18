8b164cae773c63ab8d3eec0ff278d9b27eed60b5
Andreas Stuehrk <andy-python@hammerhartes.de>
2009-09-18 21:15:18 +0200

    Remove some unused imports.

--- bpython/config.py
+++ bpython/config.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from ConfigParser import ConfigParser, NoSectionError, NoOptionError
+from ConfigParser import ConfigParser
 from itertools import chain
 from bpython.keys import key_dispatch
 import errno
