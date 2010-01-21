252c5104f99963e057407a45268836936316df2e
Simon de Vlieger <simon@ikanobori.jp>
2010-01-21 22:43:41 +0100

    Nice double space in statusbar, like the others

--- bpython/cli.py
+++ bpython/cli.py
@@ -1394,7 +1394,7 @@ def init_wins(scr, colors, config):
 # This should show to be configured keys from ~/.bpython/config
 #
     statusbar = Statusbar(scr, main_win, background, config,
-        " <%s> Rewind  <%s> Save  <%s> Pastebin  <%s> Pager <%s> Show Source " %
+        " <%s> Rewind  <%s> Save  <%s> Pastebin  <%s> Pager  <%s> Show Source " %
             (config.undo_key, config.save_key,
              config.pastebin_key, config.last_output_key,
              config.show_source_key),
