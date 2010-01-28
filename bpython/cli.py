d2d37025f14d888ab21a1d1569bd00d4e3399dd1
Andreas Stuehrk <andreas@bpython-interpreter.org>
2010-01-28 10:32:26 +0100

    Fix scrolling in echo().

--- bpython/cli.py
+++ bpython/cli.py
@@ -340,7 +340,7 @@ class CLIRepl(Repl):
         """Clear the wrapped lines of the current input."""
         # curses does not handle this on its own. Sad.
         height, width = self.scr.getmaxyx()
-        max_y = min(self.iy + len(self.s) // width + 1, height)
+        max_y = min(self.iy + (self.ix + len(self.s)) // width + 1, height)
         for y in xrange(self.iy + 1, max_y):
             self.scr.move(y, 0)
             self.scr.clrtoeol()
@@ -459,6 +459,13 @@ class CLIRepl(Repl):
         # Replace NUL bytes, as addstr raises an exception otherwise
         s = s.replace('\x00', '')
 
+        screen_height, screen_width = self.scr.getmaxyx()
+        if self.iy >= (screen_height - 1):
+            lines = (self.ix + len(s)) // screen_width
+            if lines > 0:
+                self.scr.scroll(lines)
+                self.iy -= lines
+                self.scr.move(self.iy, self.ix)
         self.scr.addstr(s, a)
 
         if redraw and not self.evaluating:
