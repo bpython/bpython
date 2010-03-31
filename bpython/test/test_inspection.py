aadc7e9ce2c88930b463000f885b5acf190cdc1d
Andreas Stuehrk <andreas@bpython-interpreter.org>
2010-03-31 15:08:34 +0200

    A failing test for issue #109.

--- bpython/test/test_inspection.py
+++ bpython/test/test_inspection.py
@@ -29,3 +29,15 @@ class TestInspection(unittest.TestCase):
         self.assertFalse(inspection.is_callable(Noncallable()))
         self.assertFalse(inspection.is_callable(OldNoncallable()))
         self.assertFalse(inspection.is_callable(None))
+
+    def test_parsekeywordpairs(self):
+        def fails(spam=['-a', '-b']):
+            pass
+
+        default_arg_repr = "['-a', '-b']"
+        self.assertEqual(str(['-a', '-b']), default_arg_repr,
+                         'This test is broken (repr does not match), fix me.')
+
+        argspec = inspection.getargspec('fails', fails)
+        defaults = argspec[1][3]
+        self.assertEqual(str(defaults[0]), default_arg_repr)
