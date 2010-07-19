3c98536ab9578a524cbd8a9560d49886ce1b564d
Michele Orrù <maker.py@gmail.com>
2010-07-20 00:13:46 +0200

    i18n: more dynamic translations loading

--- bpython/translations.py
+++ bpython/translations.py
@@ -1,4 +1,19 @@
+import os.path
 import gettext
 
-translation = gettext.translation('bpython')
-_ = translation.ugettext
+try:
+    translation = gettext.translation('bpython')
+except IOError:
+    # try to load .mo files created with 'python setup.py build_translation'
+    # from the build/ directory
+    try:
+        translation = gettext.translation('bpython',
+                            os.path.join('build', 'share', 'locale'))
+    except IOError:
+        translation = None
+
+if translation is None:
+    def _(s):
+        return s
+else:
+    _ = translation.ugettext
