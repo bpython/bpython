9f1f8acab637f0d713445bf1bba1dd47f07c3256
Michele Orrù <maker.py@gmail.com>
2010-07-21 10:41:52 +0200

    [tmpush] i18n: moving to babel

--- setup.py
+++ setup.py
@@ -27,48 +27,27 @@ except ImportError:
     from distutils.command.build_py import build_py
 
 try:
-    import msgfmt
+    from babel.messages.frontend import compile_catalog as _compile_catalog
+    from babel.messages.frontend import extract_messages# as _extract_messages
+
+    class compile_catalog(_compile_catalog):
+        def initialize_options(self):
+            """Simply set default domain and directory attributes to the
+            correct path for bpython."""
+            _compile_catalog.initialize_options(self)
+
+            self.domain = 'bpython'
+            self.directory = os.path.join('i18n', 'locale')
+
+    build.sub_commands.append(('compile_catalog', None))
+    using_translations = True
 except ImportError:
-    pass
+    using_translations = False
 
 if platform.system() == 'FreeBSD':
     man_dir = 'man'
 else:
     man_dir = 'share/man'
-
-
-
-class build_translation(cmd.Command):
-    """Internationalization suport for bpython.
-    Compile .po files into .mo files"""
-
-    description = __doc__
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        for src in glob.iglob(os.path.join('po', '[a-z][a-z]_[A-Z][A-Z].po')):
-
-            # remove 'po/' (the directory) and '.po' (the extension)
-            lang = src[3:-3]
-            dest_path = os.path.join('build', 'share', 'locale',
-                                     lang, 'LC_MESSAGES', '')
-            dest = dest_path + 'bpython.mo'
-
-            if not os.path.exists(dest_path):
-                os.makedirs(dest_path)
-            if (not os.path.exists(dest) or
-                os.stat(src)[8] > os.stat(dest)[8]):
-                print ('Adding translation: %s' % lang)
-                msgfmt.make(src, dest)
-
-build.sub_commands.insert(0, ('build_translation', None))
-
 
 data_files = [
         # man pages
@@ -78,9 +57,10 @@ data_files = [
         (os.path.join('share', 'applications'), ['data/bpython.desktop']),
 ]
 # localization
-for langfile in glob.iglob(os.path.join('po', '[a-z][a-z]_[A-Z][A-Z].po')):
-    lang_path = os.path.join('share', 'locale', langfile[3:-3], 'LC_MESSAGES')
-    data_files.append((lang_path, ['build/%s/bpython.mo' % lang_path]))
+if using_translations:
+    for lang in os.listdir('i18n'):
+        lang_path = os.path.join('', 'locale', lang, 'LC_MESSAGES', 'bpython.mo')
+        data_files.append(('share'+lang_path, ['i18n'+lang_path]))
 
 
 setup(
@@ -107,9 +87,10 @@ setup(
     },
     scripts = ([] if using_setuptools else ['data/bpython',
                                             'data/bpython-gtk']),
-    cmdclass=dict(build_py=build_py,
-                  build = build,
-                  build_translation = build_translation)
+    cmdclass = dict(build_py=build_py,
+                    build = build,
+                    compile_catalog = compile_catalog,
+                    extract_messages = extract_messages)
 )
 
 # vim: encoding=utf-8 sw=4 ts=4 sts=4 ai et sta
