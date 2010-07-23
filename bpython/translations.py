afb66c6a7dfac7c2493b09d2f1d866f60e263695
Michele Orrù <maker.py@gmail.com>
2010-07-23 16:34:19 +0200

    Syntax Error -.-

--- bpython/translations.py
+++ bpython/translations.py
@@ -6,7 +6,7 @@ try:
 except IOError:
     # try to load .mo files created with babel on i18n/ dir
     try:
-        translation = gettext.translation('bpython', 'i18n'))
+        translation = gettext.translation('bpython', 'i18n')
     except IOError:
         translation = None
 
