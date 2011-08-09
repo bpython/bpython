deb483664e1401a19ae0b3c3e41d290c8660f191
Simon de Vlieger <simon@ikanobori.jp>
2011-08-09 09:52:21 +0000

    Set the correct year for all copyrights and the -V flag

--- bpython/args.py
+++ bpython/args.py
@@ -83,7 +83,7 @@ def parse(args, extras=None, ignore_stdin=False):
     if options.version:
         print 'bpython version', __version__,
         print 'on top of Python', sys.version.split()[0]
-        print ('(C) 2008-2010 Bob Farrell, Andreas Stuehrk et al. '
+        print ('(C) 2008-2011 Bob Farrell, Andreas Stuehrk et al. '
                'See AUTHORS for detail.')
         raise SystemExit
 
