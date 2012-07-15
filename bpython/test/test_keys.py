9a93c02e7ee8c4dc04e4659b2fb9504424960e15
Sebastian Ramacher <sebastian+dev@ramacher.at>
2012-07-15 21:06:21 +0200

    Fix test_keys.

--- bpython/test/test_keys.py
+++ bpython/test/test_keys.py
@@ -1,36 +1,68 @@
 import unittest
 import bpython.keys as keys
 
-class TestKeys(unittest.TestCase):
+class TestCLIKeys(unittest.TestCase):
     def test_keymap_map(self):
         """Verify KeyMap.map being a dictionary with the correct
         length."""
-        self.assertEqual(len(keys.key_dispatch.map), 43)
+        self.assertEqual(len(keys.cli_key_dispatch.map), 43)
 
     def test_keymap_setitem(self):
         """Verify keys.KeyMap correctly setting items."""
-        keys.key_dispatch['simon'] = 'awesome';
-        self.assertEqual(keys.key_dispatch['simon'], 'awesome')
+        keys.cli_key_dispatch['simon'] = 'awesome';
+        self.assertEqual(keys.cli_key_dispatch['simon'], 'awesome')
 
     def test_keymap_delitem(self):
         """Verify keys.KeyMap correctly removing items."""
-        keys.key_dispatch['simon'] = 'awesome'
-        del keys.key_dispatch['simon']
-        if 'simon' in keys.key_dispatch.map:
+        keys.cli_key_dispatch['simon'] = 'awesome'
+        del keys.cli_key_dispatch['simon']
+        if 'simon' in keys.cli_key_dispatch.map:
             raise Exception('Key still exists in dictionary')
 
     def test_keymap_getitem(self):
         """Verify keys.KeyMap correctly looking up items."""
-        self.assertEqual(keys.key_dispatch['C-['], (chr(27), '^['))
-        self.assertEqual(keys.key_dispatch['F11'], ('KEY_F(11)',))
-        self.assertEqual(keys.key_dispatch['C-a'], ('\x01', '^A'))
+        self.assertEqual(keys.cli_key_dispatch['C-['], (chr(27), '^['))
+        self.assertEqual(keys.cli_key_dispatch['F11'], ('KEY_F(11)',))
+        self.assertEqual(keys.cli_key_dispatch['C-a'], ('\x01', '^A'))
 
     def test_keymap_keyerror(self):
         """Verify keys.KeyMap raising KeyError when getting undefined key"""
         def raiser():
-            keys.key_dispatch['C-asdf']
-            keys.key_dispatch['C-qwerty']
+            keys.cli_key_dispatch['C-asdf']
+            keys.cli_key_dispatch['C-qwerty']
         self.assertRaises(KeyError, raiser);
+
+class TestUrwidKeys(unittest.TestCase):
+    def test_keymap_map(self):
+        """Verify KeyMap.map being a dictionary with the correct
+        length."""
+        self.assertEqual(len(keys.urwid_key_dispatch.map), 64)
+
+    def test_keymap_setitem(self):
+        """Verify keys.KeyMap correctly setting items."""
+        keys.urwid_key_dispatch['simon'] = 'awesome';
+        self.assertEqual(keys.urwid_key_dispatch['simon'], 'awesome')
+
+    def test_keymap_delitem(self):
+        """Verify keys.KeyMap correctly removing items."""
+        keys.urwid_key_dispatch['simon'] = 'awesome'
+        del keys.urwid_key_dispatch['simon']
+        if 'simon' in keys.urwid_key_dispatch.map:
+            raise Exception('Key still exists in dictionary')
+
+    def test_keymap_getitem(self):
+        """Verify keys.KeyMap correctly looking up items."""
+        self.assertEqual(keys.urwid_key_dispatch['F11'], 'f11')
+        self.assertEqual(keys.urwid_key_dispatch['C-a'], 'ctrl a')
+        self.assertEqual(keys.urwid_key_dispatch['M-a'], 'meta a')
+
+    def test_keymap_keyerror(self):
+        """Verify keys.KeyMap raising KeyError when getting undefined key"""
+        def raiser():
+            keys.urwid_key_dispatch['C-asdf']
+            keys.urwid_key_dispatch['C-qwerty']
+        self.assertRaises(KeyError, raiser);
+
 
 if __name__ == '__main__':
     unittest.main()
