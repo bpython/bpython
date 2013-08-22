f18db22d988ac29acbb348e5927dacfa55eac4f9
Thomas Ballinger <thomasballinger@gmail.com>
2013-08-22 12:39:46 -0400

    optionally update abbreviations with vim abbrs - not wired up yet
    functionality added, can't be used yet
    
    --HG--
    branch : scroll-frontend
    extra : rebase_source : 936978d241502c2f7a0f75cb7962de4c74a43111

--- bpython/scrollfrontend/abbreviate.py
+++ bpython/scrollfrontend/abbreviate.py
@@ -1,3 +1,9 @@
+import os
+from subprocess import Popen
+import tempfile
+import shutil
+import re
+
 ABBR = {
         'improt' : 'import',
         'imprt'  : 'import',
@@ -5,14 +11,36 @@ ABBR = {
         'form'   : 'from',
         }
 
-import re
-
 def substitute_abbreviations(cursor_offset, line):
     """This should be much better"""
     new_line = ''.join([ABBR.get(word, word) for word in re.split(r'(\w+)', line)])
     cursor_offset = cursor_offset + len(new_line) - len(line)
     return cursor_offset, new_line
 
+
+def python_vim_abbreviations():
+    fn = tempfile.mkdtemp()
+    mock_python_filename = os.path.join(fn, 'python.py')
+    abbreviations_filename = os.path.join(fn, 'abbrs')
+
+    p = Popen(['vim', '-c', 'redir > %s|abbreviate|redir END|q' % abbreviations_filename, mock_python_filename])
+    p.communicate()
+    s = open(abbreviations_filename).read()
+    shutil.rmtree(fn)
+
+    def clean_line(line):
+        m = re.search(r'[!]\W*(\w+)\W*[@]?(\w+)\W*', line)
+        return m.groups()
+
+    return dict([clean_line(line) for line in s.split('\n') if line.strip()])
+
+def update_abbrs_with_vim_abbrs():
+    abbrs = python_vim_abbreviations()
+    ABBR.update(abbrs)
+
 if __name__ == '__main__':
+    abbrs = python_vim_abbreviations()
+    print abbrs
+    ABBR.update(abbrs)
     print(substitute_abbreviations(0, 'improt asdf'))
     print(substitute_abbreviations(0, 'foo(x, y() - 2.3242) + "asdf"'))
