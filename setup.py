2f41e6d383c6f714e91c63cba6273912ef852aba
Thomas Ballinger <thomasballinger@gmail.com>
2013-10-14 11:04:58 -0400

    bumped fmtstr required version to 0.0.9
    --HG--
    branch : scroll-frontend

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ setup(
         'pygments'
     ],
     extras_require = {
-        'scroll': ['fmtstr>=0.0.4']
+        'scroll': ['fmtstr>=0.0.9']
     },
     tests_require = ['mock'],
     packages = ["bpython", "bpython.test", "bpython.translations", "bpdb", "bpython.scrollfrontend"],
