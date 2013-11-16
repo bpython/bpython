9f98317f3bd5009e7a26ed3832e116defcdb5cb5
Thomas Ballinger <thomasballinger@gmail.com>
2013-11-16 11:45:01 -0500

    added clarifying comment to path add
    --HG--
    branch : scroll-frontend

--- bpython/scroll.py
+++ bpython/scroll.py
@@ -42,7 +42,7 @@ def main(args=None, locals_=None, banner=None):
                         term.render_to_terminal(array, cursor_pos)
                         raise SystemExit(exit_value)
                 else:
-                    sys.path.insert(0, '')
+                    sys.path.insert(0, '') # expected for interactive sessions (python does it)
 
                 while True:
                     try:
