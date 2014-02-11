c129420a29e8c8a2cb7481fb7406ad0ce323de3e
Thomas Ballinger <thomasballinger@gmail.com>
2014-02-11 18:25:27 -0500

    fix redraw of history on making screen smaller

--- bpython/curtsiesfrontend/replpainter.py
+++ bpython/curtsiesfrontend/replpainter.py
@@ -29,7 +29,7 @@ def display_linize(msg, columns):
 def paint_history(rows, columns, display_lines):
     lines = []
     for r, line in zip(range(rows), display_lines[-rows:]):
-        lines.append(fmtstr(line))
+        lines.append(fmtstr(line[:columns]))
     r = fsarray(lines, width=columns)
     assert r.shape[0] <= rows, repr(r.shape)+' '+repr(rows)
     assert r.shape[1] <= columns, repr(r.shape)+' '+repr(columns)
