2924d925816bb11e4a83422340d0f6e6d109a93c
Thomas Ballinger <thomasballinger@gmail.com>
2014-02-16 12:34:50 -0500

    --type option for simulating typed input

--- bpython/curtsies.py
+++ bpython/curtsies.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
 import sys
+import os
 from optparse import Option
 
 import curtsies
 import curtsies.window
 import curtsies.terminal
+import curtsies.events
 Window = curtsies.window.Window
 Terminal = curtsies.terminal.Terminal
 
@@ -19,6 +21,8 @@ def main(args=None, locals_=None, banner=None):
         'scroll options', None, [
             Option('--log', '-L', action='store_true',
                 help=_("log debug messages to scroll.log")),
+            Option('--type', '-t', action='store_true',
+                help=_("enter lines of file as though interactively typed")),
             ]))
     if options.log:
         import logging
@@ -36,26 +40,9 @@ def main(args=None, locals_=None, banner=None):
                 repl.width = columns
                 repl.height = rows
 
-                exit_value = 0
-                if exec_args:
-                    assert options, "don't pass in exec_args without options"
+                def process_event(e):
                     try:
-                        # THIS IS NORMAL PYTHON
-                        #TODO replace this so that stdout is properly harvested for display and rewind works
-                        bpargs.exec_code(repl.interp, exec_args)
-                    except SystemExit, e:
-                        exit_value = e.args
-                    if not options.interactive:
-                        #TODO treat this properly: no prompt should ever display, but stdout should!
-                        array, cursor_pos = repl.paint(about_to_exit=True)
-                        term.render_to_terminal(array, cursor_pos)
-                        raise SystemExit(exit_value)
-                else:
-                    sys.path.insert(0, '') # expected for interactive sessions (python does it)
-
-                while True:
-                    try:
-                        repl.process_event(tc.get_event())
+                        repl.process_event(e)
                     except SystemExitFromCodeThread:
                         #Get rid of nasty constant
                         array, cursor_pos = repl.paint(about_to_exit=2)
@@ -71,6 +58,35 @@ def main(args=None, locals_=None, banner=None):
                         repl.scroll_offset += scrolled
                         # Could this be calculated in the repl, avoiding this
                         # two-way communication?
+
+                exit_value = 0
+                if exec_args:
+                    assert options, "don't pass in exec_args without options"
+                    if options.type:
+                        repl.process_event(tc.get_event()) #first event will always be a window size set
+                        paste = curtsies.events.PasteEvent()
+                        old_argv, sys.argv = sys.argv, exec_args
+                        paste.events.extend(open(exec_args[0]).read())
+                        sys.path.insert(0, os.path.abspath(os.path.dirname(exec_args[0])))
+                        process_event(paste)
+                    else:
+                        try:
+                            # THIS IS NORMAL PYTHON
+                            #TODO replace this so that stdout is properly harvested for display and rewind works
+                            bpargs.exec_code(repl.interp, exec_args)
+                        except SystemExit, e:
+                            exit_value = e.args
+                        if not options.interactive:
+                            #TODO treat this properly: no prompt should ever display, but stdout should!
+                            array, cursor_pos = repl.paint(about_to_exit=True)
+                            term.render_to_terminal(array, cursor_pos)
+                            raise SystemExit(exit_value)
+                else:
+                    sys.path.insert(0, '') # expected for interactive sessions (python does it)
+
+                while True:
+                    process_event(tc.get_event())
+
 
 if __name__ == '__main__':
     sys.exit(main())
