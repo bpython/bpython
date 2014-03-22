01e0f84ca0f6a9d6396fb163ff298505a97172d1
Thomas Ballinger <thomasballinger@gmail.com>
2014-03-22 14:13:37 -0400

    shorten up main

--- bpython/curtsies.py
+++ bpython/curtsies.py
@@ -46,7 +46,7 @@ def main(args=None, locals_=None, banner=None):
             if not options.interactive:
                 raise SystemExit(exit_value)
     else:
-        sys.path.insert(0, '') # expected for interactive sessions (python does it)
+        sys.path.insert(0, '') # expected for interactive sessions (vanilla python does it)
 
     mainloop(config, locals_, banner, interp, paste)
 
@@ -65,12 +65,8 @@ def mainloop(config, locals_, banner, interp=None, paste=None):
                 def process_event(e):
                     try:
                         repl.process_event(e)
-                    except SystemExitFromCodeGreenlet:
-                        array, cursor_pos = repl.paint(about_to_exit=True, user_quit=True)
-                        term.render_to_terminal(array, cursor_pos)
-                        raise
-                    except SystemExit:
-                        array, cursor_pos = repl.paint(about_to_exit=True)
+                    except (SystemExitFromCodeGreenlet, SystemExit) as err:
+                        array, cursor_pos = repl.paint(about_to_exit=True, user_quit=isinstance(err, SystemExitFromCodeGreenlet))
                         term.render_to_terminal(array, cursor_pos)
                         raise
                     else:
