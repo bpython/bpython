aea557af6f26db8e1ec5766756c7339d06cac965
Thomas Ballinger <thomasballinger@gmail.com>
2014-08-29 09:11:04 -0700

    fix #366 and fix #367
    adds tests for and fixes external communication, and
    turns off welcome banner if help key is disabled

--- bpython/test/test_curtsies_repl.py
+++ bpython/test/test_curtsies_repl.py
@@ -1,13 +1,28 @@
 import unittest
 import sys
+import os
 py3 = (sys.version_info[0] == 3)
 
 from bpython.curtsiesfrontend import repl
+from bpython import config
+
+def setup_config(conf):
+    config_struct = config.Struct()
+    config.loadini(config_struct, os.devnull)
+    for key, value in conf.items():
+        if not hasattr(config_struct, key):
+            raise ValueError("%r is not a valid config attribute", (key,))
+        setattr(config_struct, key, value)
+    return config_struct
 
 class TestCurtsiesRepl(unittest.TestCase):
 
     def setUp(self):
-        self.repl = repl.Repl()
+        self.config = setup_config({'editor':'true'})
+        self.repl = repl.Repl(config=self.config)
+        os.environ['PAGER'] = 'true'
+        self.repl.width = 50
+        self.repl.height = 20
 
     def test_buffer_finished_will_parse(self):
         self.repl.buffer = ['1 + 1']
@@ -22,6 +37,12 @@ class TestCurtsiesRepl(unittest.TestCase):
         self.assertTrue(self.repl.buffer_finished_will_parse(), (True, True))
         self.repl.buffer = ['def foo(x):', '    return 1', '']
         self.assertTrue(self.repl.buffer_finished_will_parse(), (True, True))
+
+    def test_external_communication(self):
+        self.assertEqual(type(self.repl.help_text()), type(b''))
+        self.repl.send_current_block_to_external_editor()
+        self.repl.send_session_to_external_editor()
+
 
 if __name__ == '__main__':
     unittest.main()
