3fa21ab77927347587974cd312254bcc1cc33889
Thomas Ballinger <thomasballinger@gmail.com>
2014-10-16 11:29:11 -0400

    fix Python 3 test

--- bpython/test/test_args.py
+++ bpython/test/test_args.py
@@ -10,11 +10,11 @@ class TestExecArgs(unittest.TestCase):
     def test_exec_dunder_file(self):
         with tempfile.NamedTemporaryFile(delete=False) as f:
             f.write(
-            "import sys; print 'hello'; sys.stderr.write(__file__); sys.stderr.flush();")
+            "import sys; sys.stderr.write(__file__); sys.stderr.flush();".encode('ascii'))
             f.flush()
             print open(f.name).read()
-            p = subprocess.Popen(['bpython-curtsies', f.name], stderr= subprocess.PIPE)
-            
-            self.assertEquals(p.stderr.read().strip(), f.name)
+            p = subprocess.Popen(['bpython-curtsies', f.name], stderr=subprocess.PIPE)
+
+            self.assertEquals(p.stderr.read().strip().decode('ascii'), f.name)
 
 
