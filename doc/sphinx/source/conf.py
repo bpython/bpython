717206cfe21ee89fa1d2afdf3f756621db2d617f
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-01-09 16:44:16 +0100

    There is no tex documentaiton
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -171,10 +171,10 @@ htmlhelp_basename = 'bpythondoc'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'bpython.tex', u'bpython Documentation',
-   u'Robert Farrell', 'manual'),
-]
+#latex_documents = [
+#  ('index', 'bpython.tex', u'bpython Documentation',
+#   u'Robert Farrell', 'manual'),
+#]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
