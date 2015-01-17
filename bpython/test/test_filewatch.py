90e2bbea53b7de2801db90f3b0a2df1d99c3821d
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-01-17 14:55:14 +0100

    Skip watchdog tests if watchdog is not installed
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- bpython/test/test_filewatch.py
+++ bpython/test/test_filewatch.py
@@ -6,25 +6,29 @@ try:
 except ImportError:
     import unittest
 
-from bpython.curtsiesfrontend.filewatch import ModuleChangedEventHandler
+try:
+    from bpython.curtsiesfrontend.filewatch import ModuleChangedEventHandler
+    has_watchdog = True
+except ImportError:
+    has_watchdog = False
 
+@unittest.skipIf(not has_watchdog, "watchdog not available")
 class TestModuleChangeEventHandler(unittest.TestCase):
-    
+
     def setUp(self):
         self.module = ModuleChangedEventHandler([], 1)
         self.module.observer = mock.Mock()
-        
+
     def test_create_module_handler(self):
         self.assertIsInstance(self.module, ModuleChangedEventHandler)
-        
+
     def test_add_module(self):
         self.module._add_module('something/test.py')
-        self.assertIn(os.path.abspath('something/test'), 
+        self.assertIn(os.path.abspath('something/test'),
                       self.module.dirs[os.path.abspath('something')])
-    
+
     def test_activate_throws_error_when_already_activated(self):
         self.module.activated = True
         with self.assertRaises(ValueError):
             self.module.activate()
-    
-    
+
