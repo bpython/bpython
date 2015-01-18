30c0dcbe1dd4a183f0e9bd1a795da9b8eef32f43
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-01-18 14:15:41 +0100

    Run nosetests with -v
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- .travis.script.sh
+++ .travis.script.sh
@@ -7,5 +7,5 @@ if [[ $RUN == build_sphinx ]] then
     python setup.py build_sphinx_man
 elif [[ $RUN == nosetests ]] then
   cd build/lib/
-  nosetests bpython/test
+  nosetests -v bpython/test
 fi
