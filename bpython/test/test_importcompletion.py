0badec0509b66b3b54833a23317d0de4299c1a05
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-01-21 16:08:31 +0100

    Newlines
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- bpython/test/test_importcompletion.py
+++ bpython/test/test_importcompletion.py
@@ -3,32 +3,40 @@ from bpython import importcompletion
 import unittest
 
 class TestSimpleComplete(unittest.TestCase):
+
     def setUp(self):
         self.original_modules = importcompletion.modules
         importcompletion.modules = ['zzabc', 'zzabd', 'zzefg', 'zzabc.e', 'zzabc.f']
+
     def tearDown(self):
         importcompletion.modules = self.original_modules
+
     def test_simple_completion(self):
         self.assertEqual(importcompletion.complete(10, 'import zza'), ['zzabc', 'zzabd'])
+
     def test_package_completion(self):
         self.assertEqual(importcompletion.complete(13, 'import zzabc.'), ['zzabc.e', 'zzabc.f', ])
 
 
 class TestRealComplete(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         [_ for _ in importcompletion.find_iterator]
         __import__('sys')
         __import__('os')
+
     @classmethod
     def tearDownClass(cls):
         importcompletion.find_iterator = importcompletion.find_all_modules()
         importcompletion.modules = set()
+
     def test_from_attribute(self):
         self.assertEqual(importcompletion.complete(19, 'from sys import arg'), ['argv'])
+
     def test_from_attr_module(self):
         self.assertEqual(importcompletion.complete(9, 'from os.p'), ['os.path'])
+
     def test_from_package(self):
         self.assertEqual(importcompletion.complete(17, 'from xml import d'), ['dom'])
-
 
