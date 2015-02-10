26d2065751e542e1c49caa562e8ee97b22e16e73
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-02-10 01:04:19 +0100

    Add missing unicode_literals import
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- bpython/history.py
+++ bpython/history.py
@@ -23,7 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
+from __future__ import unicode_literals
 import io
 import os
 from itertools import islice
