a4b174cfdfc37054d96e2345c789e4b837b2db6f
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-15 19:57:51 -0500

    fix python 3

--- bpython/test/test_inspection.py
+++ bpython/test/test_inspection.py
@@ -2,6 +2,8 @@
 
 import os
 
+
+from bpython._py3compat import py3
 from bpython import inspection
 from bpython.test import unittest
 from bpython.test.fodder import encoding_ascii
@@ -59,13 +61,24 @@ class TestInspection(unittest.TestCase):
         self.assertFalse(inspection.is_callable(None))
         self.assertTrue(inspection.is_callable(CallableMethod().method))
 
-    def test_is_new_style(self):
+    @unittest.skipIf(py3, 'old-style classes only exist in Python 2')
+    def test_is_new_style_py2(self):
         self.assertTrue(inspection.is_new_style(spam))
         self.assertTrue(inspection.is_new_style(Noncallable))
         self.assertFalse(inspection.is_new_style(OldNoncallable))
         self.assertTrue(inspection.is_new_style(Noncallable()))
         self.assertFalse(inspection.is_new_style(OldNoncallable()))
         self.assertTrue(inspection.is_new_style(None))
+
+    @unittest.skipUnless(py3, 'only in Python 3 are all classes new-style')
+    def test_is_new_style_py3(self):
+        self.assertTrue(inspection.is_new_style(spam))
+        self.assertTrue(inspection.is_new_style(Noncallable))
+        self.assertTrue(inspection.is_new_style(OldNoncallable))
+        self.assertTrue(inspection.is_new_style(Noncallable()))
+        self.assertTrue(inspection.is_new_style(OldNoncallable()))
+        self.assertTrue(inspection.is_new_style(None))
+
 
     def test_parsekeywordpairs(self):
         # See issue #109
@@ -169,6 +182,7 @@ class TestSafeGetAttribute(unittest.TestCase):
         self.assertEquals(inspection.safe_get_attribute_new_style(p, 'prop'),
                           Property.prop)
 
+    @unittest.skipIf(py3, 'Old-style classes not in Python 3')
     def test_raises_on_old_style_class(self):
         class Old: pass
         with self.assertRaises(ValueError):
