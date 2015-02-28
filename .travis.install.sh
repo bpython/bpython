62eccc7527422e6cfd50e5e979897338b46e8927
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-02-28 12:52:47 +0100

    Remove curtsies restriction
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- .travis.install.sh
+++ .travis.install.sh
@@ -8,7 +8,7 @@ if [[ $RUN == nosetests ]]; then
     # core dependencies
     pip install pygments
     pip install requests
-    pip install 'curtsies >=0.1.17,<0.2.0'
+    pip install 'curtsies >=0.1.17'
     pip install greenlet
     pip install 'six >=1.4'
     # filewatch specific dependencies
