27b788cb4e54fdaa703d485ab7dcda565224240a
noella <noelladsa@gmail.com>
2015-06-08 16:27:19 -0400

    Added accidentally deleted Copyright

--- bpython/autocomplete.py
+++ bpython/autocomplete.py
@@ -2,6 +2,7 @@
 
 # The MIT License
 #
+# Copyright (c) 2009-2011 the bpython authors.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
