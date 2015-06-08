2141cf78bcd58ef4978935494a1fc904e7a57380
noella <noelladsa@gmail.com>
2015-06-08 16:35:45 -0400

    Updated copyright year

--- bpython/autocomplete.py
+++ bpython/autocomplete.py
@@ -2,7 +2,7 @@
 
 # The MIT License
 #
-# Copyright (c) 2009-2011 the bpython authors.
+# Copyright (c) 2009-2015 the bpython authors.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
