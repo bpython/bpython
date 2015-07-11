dc0f2945b2176d4741a8b8688105972809c4c7e7
sharow <shirowmiura@gmail.com>
2015-07-11 17:35:32 +0900

    add test

--- bpython/test/test_curtsies_repl.py
+++ bpython/test/test_curtsies_repl.py
@@ -6,6 +6,7 @@ import os
 import sys
 import tempfile
 import io
+from functools import partial
 from contextlib import contextmanager
 from six.moves import StringIO
 
@@ -282,6 +283,68 @@ class TestCurtsiesReevaluate(TestCase):
         self.assertIn('b', self.repl.interp.locals)
         self.repl.undo()
         self.assertNotIn('b', self.repl.interp.locals)
+
+
+class TestCurtsiesReevaluateWithImport(TestCase):
+    def setUp(self):
+        self.repl = create_repl()
+        self.open = partial(io.open, mode='wt', encoding='utf-8')
+        self.dont_write_bytecode = sys.dont_write_bytecode
+        sys.dont_write_bytecode = True
+
+    def tearDown(self):
+        sys.dont_write_bytecode = self.dont_write_bytecode
+
+    def push(self, line):
+        self.repl._current_line = line
+        self.repl.on_enter()
+
+    def head(self, path):
+        self.push('import sys')
+        self.push('sys.path.append("%s")' % (path))
+
+    @staticmethod
+    @contextmanager
+    def tempfile():
+        with tempfile.NamedTemporaryFile(suffix='.py') as temp:
+            path, name = os.path.split(temp.name)
+            yield temp.name, path, name.replace('.py', '')
+
+    def test_module_content_changed(self):
+        with self.tempfile() as (fullpath, path, modname):
+            with self.open(fullpath) as f:
+                f.write('a = 0\n')
+            self.head(path)
+            self.push('import %s' % (modname))
+            self.push('a = %s.a' % (modname))
+            self.assertIn('a', self.repl.interp.locals)
+            self.assertEqual(self.repl.interp.locals['a'], 0)
+            with self.open(fullpath) as f:
+                f.write('a = 1\n')
+            self.repl.clear_modules_and_reevaluate()
+            self.assertIn('a', self.repl.interp.locals)
+            self.assertEqual(self.repl.interp.locals['a'], 1)
+
+    def test_import_module_with_rewind(self):
+        with self.tempfile() as (fullpath, path, modname):
+            with self.open(fullpath) as f:
+                f.write('a = 0\n')
+            self.head(path)
+            self.push('import %s' % (modname))
+            self.assertIn(modname, self.repl.interp.locals)
+            self.repl.undo()
+            self.assertNotIn(modname, self.repl.interp.locals)
+            self.repl.clear_modules_and_reevaluate()
+            self.assertNotIn(modname, self.repl.interp.locals)
+            self.push('import %s' % (modname))
+            self.push('a = %s.a' % (modname))
+            self.assertIn('a', self.repl.interp.locals)
+            self.assertEqual(self.repl.interp.locals['a'], 0)
+            with self.open(fullpath) as f:
+                f.write('a = 1\n')
+            self.repl.clear_modules_and_reevaluate()
+            self.assertIn('a', self.repl.interp.locals)
+            self.assertEqual(self.repl.interp.locals['a'], 1)
 
 
 class TestCurtsiesPagerText(TestCase):
