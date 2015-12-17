561954415c585e21ad15e5691a54d0888d74b5c0
Thomas Ballinger <thomasballinger@gmail.com>
2015-12-17 10:34:54 -0500

    reset stdin/out/err when reloading sys
    fixes #567

--- bpython/curtsiesfrontend/sitefix.py
+++ bpython/curtsiesfrontend/sitefix.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 
 from six.moves import builtins
 
@@ -16,3 +17,26 @@ def resetquit(builtins):
 def monkeypatch_quit():
     if 'site' in sys.modules:
         resetquit(builtins)
+
+
+orig_reload = builtins.reload
+
+
+def reload(module):
+    if module is sys:
+        orig_stdout = sys.stdout
+        orig_stderr = sys.stderr
+        orig_stdin = sys.stdin
+        orig_reload(sys)
+        sys.stdout = orig_stdout
+        sys.stderr = orig_stderr
+        sys.stdin = orig_stdin
+    else:
+        builtins.reload(sys)
+
+
+functools.update_wrapper(reload, orig_reload)
+
+
+def monkeypatch_reload():
+    builtins.reload = reload
