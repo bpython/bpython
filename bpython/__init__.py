f2014dbae31313571cc9c26f51a14f4fda09d138
Josh Soref <jsoref@users.noreply.github.com>
2016-04-07 16:35:33 +0000

    spelling: unknown

--- bpython/__init__.py
+++ bpython/__init__.py
@@ -25,7 +25,7 @@ import os.path
 try:
     from bpython._version import __version__ as version
 except ImportError:
-    version = 'unkown'
+    version = 'unknown'
 
 __version__ = version
 package_dir = os.path.abspath(os.path.dirname(__file__))
