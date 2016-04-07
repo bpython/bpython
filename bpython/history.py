0f5dff3e4a439c4af9313ec24b62131d38907854
Josh Soref <jsoref@users.noreply.github.com>
2016-04-07 16:27:48 +0000

    spelling: copyright

--- bpython/history.py
+++ bpython/history.py
@@ -3,7 +3,7 @@
 # The MIT License
 #
 # Copyright (c) 2009 the bpython authors.
-# Copyirhgt (c) 2012,2015 Sebastian Ramacher
+# Copyright (c) 2012,2015 Sebastian Ramacher
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
