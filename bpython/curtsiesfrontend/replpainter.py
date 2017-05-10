cc9fbb4115b9041b0754ffdd6a3b0ace4b0a0a7d
Attila Szöllősi <ata@openmailbox.org>
2017-05-10 22:55:16 +0200

    Use width aware slice

--- bpython/curtsiesfrontend/replpainter.py
+++ bpython/curtsiesfrontend/replpainter.py
@@ -25,11 +25,14 @@ def display_linize(msg, columns, blank_line=False):
     """Returns lines obtained by splitting msg over multiple lines.
 
     Warning: if msg is empty, returns an empty list of lines"""
-    display_lines = ([msg[start:end]
-                      for start, end in zip(
-                          range(0, len(msg), columns),
-                          range(columns, len(msg) + columns, columns))]
-                     if msg else ([''] if blank_line else []))
+    msg = fmtstr(msg)
+    try:
+        display_lines = [msg.width_aware_slice(slice(start, end))
+                          for start, end in zip(
+                              range(0, msg.width, columns),
+                              range(columns, msg.width + columns, columns))]
+    except ValueError:
+        display_lines = ([''] if blank_line else [])
     return display_lines
 
 
