5f15d7d61e1b95a01db126123afa8e6b2308686b
Etienne Richart <etiennerichart@gmail.com>
2020-07-13 08:22:00 -0500

    removed chdir and made better test skip

--- bpython/test/test_import_not_cyclical.py
+++ bpython/test/test_import_not_cyclical.py
@@ -2,79 +2,79 @@ from bpython.test import unittest
 from bpython.importcompletion import find_modules
 import os, sys, tempfile
 
-if sys.version_info[0] > 2:
-    class TestAvoidSymbolicLinks(unittest.TestCase):
-        def setUp(self):
-            with tempfile.TemporaryDirectory() as import_test_folder:
-                os.chdir(import_test_folder)
+@unittest.skipIf(sys.version_info[0] <= 2, "Test doesn't work in python 2.")
+class TestAvoidSymbolicLinks(unittest.TestCase):
+    def setUp(self):
+        with tempfile.TemporaryDirectory() as import_test_folder:
+            os.mkdir(os.path.join(import_test_folder, "Level0"))
+            os.mkdir(os.path.join(import_test_folder, "Right"))
+            os.mkdir(os.path.join(import_test_folder, "Left"))
 
-                os.mkdir(os.path.join(os.getcwd(), "Level0"))
-                os.mkdir(os.path.join(os.getcwd(), "Right"))
-                os.mkdir(os.path.join(os.getcwd(), "Left"))
-                
-                os.chdir("Level0")
-                with open("__init__.py", "w") as init_file:
-                    pass
+            current_path = os.path.join(import_test_folder, "Level0")
+            with open(
+                os.path.join(current_path, "__init__.py"), "x"
+            ) as init_file:
+                pass
 
-                os.mkdir(os.path.join(os.getcwd(), "Level1"))
-                os.chdir("Level1")
-                with open("__init__.py", "w") as init_file:
-                    pass
+            current_path = os.path.join(current_path, "Level1")
+            os.mkdir(current_path)
+            with open(
+                os.path.join(current_path, "__init__.py"), "x"
+            ) as init_file:
+                pass
 
-                os.mkdir(os.path.join(os.getcwd(), "Level2"))
-                os.chdir("Level2")
-                with open("__init__.py", "w") as init_file:
-                    pass
+            current_path = os.path.join(current_path, "Level2")
+            os.mkdir(current_path)
+            with open(
+                os.path.join(current_path, "__init__.py"), "x"
+            ) as init_file:
+                pass
 
-                os.symlink(
-                    "import_test_folder/Level0/Level1",
-                    os.path.join(os.getcwd(), "Level3"),
-                    True,
-                )
+            os.symlink(
+                "import_test_folder/Level0/Level1",
+                os.path.join(current_path, "Level3"),
+                True,
+            )
 
-                os.chdir(import_test_folder)
+            current_path = os.path.join(import_test_folder, "Right")
+            with open(
+                os.path.join(current_path, "__init__.py"), "x"
+            ) as init_file:
+                pass
 
-                os.chdir("Right")
-                with open("__init__.py", "w") as init_file:
-                    pass
+            os.symlink(
+                "import_test_folder/Left",
+                os.path.join(current_path, "toLeft"),
+                True,
+            )
 
-                os.symlink(
-                    "import_test_folder/Left",
-                    os.path.join(os.getcwd(), "toLeft"),
-                    True,
-                )
+            current_path = os.path.join(import_test_folder, "Left")
+            with open(
+                os.path.join(current_path, "__init__.py"), "x"
+            ) as init_file:
+                pass
 
-                os.chdir(import_test_folder)
+            os.symlink(
+                "import_test_folder/Right",
+                os.path.join(current_path, "toRight"),
+                True,
+            )
 
-                os.chdir("Left")
-                with open("__init__.py", "w") as init_file:
-                    pass
+            self.foo = list(
+                find_modules(os.path.abspath(import_test_folder))
+            )
+            self.filepaths = [
+                "Left",
+                "Level0.Level1.Level2",
+                "Level0.Level1",
+                "Level0",
+                "Right",
+            ]
 
-                os.symlink(
-                    "import_test_folder/Right",
-                    os.path.join(os.getcwd(), "toRight"),
-                    True,
-                )
-
-                self.foo = list(find_modules(os.path.abspath(import_test_folder)))
-                self.filepaths = [
-                    "Left",
-                    "Level0.Level1.Level2",
-                    "Level0.Level1",
-                    "Level0",
-                    "Right"
-                ]
-
-        def test_simple_symbolic_link_loop(self):
-            for thing in self.foo:
-                self.assertTrue(thing in self.filepaths)
-                self.filepaths.remove(thing)
-
-else:
-    @unittest.skip("test doesn't work in python 2")
-    def test_skip():
-        pass
-
+    def test_simple_symbolic_link_loop(self):
+        for thing in self.foo:
+            self.assertTrue(thing in self.filepaths)
+            self.filepaths.remove(thing)
 
 if __name__ == "__main__":
     unittest.main()
