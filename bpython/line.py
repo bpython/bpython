e470176e27659bf708ed72112c7d4c6d1a8a51da
Arian Deimling <arianm@deimling.net>
2021-09-28 22:27:10 -0400

    removed erroneous print()

--- bpython/line.py
+++ bpython/line.py
@@ -87,7 +87,6 @@ def current_dict(cursor_offset: int, line: str) -> Optional[LinePart]:
     """If in dictionary completion, return the dict that should be used"""
     for m in _current_dict_re.finditer(line):
         if m.start(2) <= cursor_offset <= m.end(2):
-            print(LinePart(m.start(1), m.end(1), m.group(1))) # TODO
             return LinePart(m.start(1), m.end(1), m.group(1))
     return None
 
