286d71c6f8f1e8c4fd0784eb548b9f274f754c88
Arian Deimling <arianm@deimling.net>
2021-10-01 11:17:43 -0400

    modified test to test strings containing '[' and ']'

--- bpython/test/test_line_properties.py
+++ bpython/test/test_line_properties.py
@@ -192,9 +192,9 @@ class TestCurrentDictKey(LineTestCase):
         self.assertAccess("object.dict[<-1000>|")
         self.assertAccess("object.dict[<-0.23948>|")
         self.assertAccess("object.dict[<'\U0001ffff>|")
-        self.assertAccess(r"object.dict[<'a\'\\\"\n\\'>|]")
-        self.assertAccess("object.dict[<\"abc'\">|]")
-        self.assertAccess("object.dict[<-1029.19384>|]")
+        self.assertAccess(r"object.dict[<'a\'\\\"\n\\|[[]'>")
+        self.assertAccess("object.dict[<\"a]bc[|]\">]")
+        self.assertAccess("object.dict[<'abcd[]>|")
 
 
 class TestCurrentDict(LineTestCase):
