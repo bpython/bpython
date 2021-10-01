0a08f41ee568559c9b19d6fdb3076bf900c041e8
Arian Deimling <arianm@deimling.net>
2021-10-01 19:26:44 -0400

    ran black formatter on file

--- bpython/test/test_line_properties.py
+++ bpython/test/test_line_properties.py
@@ -194,7 +194,7 @@ class TestCurrentDictKey(LineTestCase):
         self.assertAccess("object.dict[<'\U0001ffff>|")
         self.assertAccess(r"object.dict[<'a\'\\\"\n\\'>|]")
         self.assertAccess(r"object.dict[<'a\'\\\"\n\\|[[]'>")
-        self.assertAccess("object.dict[<\"a]bc[|]\">]")
+        self.assertAccess('object.dict[<"a]bc[|]">]')
         self.assertAccess("object.dict[<'abcd[]>|")
 
 
