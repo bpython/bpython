44c3b702037c1c1a5f7681b7f86c0246fbf0db7e
supakeen <cmdr@supakeen.com>
2021-11-09 16:51:56 +0000

    Remove deprecated curses (cli) rendering backend.

--- bpython/test/test_repl.py
+++ bpython/test/test_repl.py
@@ -10,7 +10,7 @@ from itertools import islice
 from pathlib import Path
 from unittest import mock
 
-from bpython import config, repl, cli, autocomplete
+from bpython import config, repl, autocomplete
 from bpython.line import LinePart
 from bpython.test import (
     MagicIterMock,
@@ -65,13 +65,6 @@ class FakeRepl(repl.Repl):
 
     def reevaluate(self):
         raise NotImplementedError
-
-
-class FakeCliRepl(cli.CLIRepl, FakeRepl):
-    def __init__(self):
-        self.s = ""
-        self.cpos = 0
-        self.rl_history = FakeHistory()
 
 
 class TestMatchesIterator(unittest.TestCase):
@@ -537,149 +530,6 @@ class TestRepl(unittest.TestCase):
         self.assertTrue(self.repl.complete())
         self.assertTrue(hasattr(self.repl.matches_iter, "matches"))
         self.assertEqual(self.repl.matches_iter.matches, ["apple2=", "apple="])
-
-
-class TestCliRepl(unittest.TestCase):
-    def setUp(self):
-        self.repl = FakeCliRepl()
-
-    def test_atbol(self):
-        self.assertTrue(self.repl.atbol())
-
-        self.repl.s = "\t\t"
-        self.assertTrue(self.repl.atbol())
-
-        self.repl.s = "\t\tnot an empty line"
-        self.assertFalse(self.repl.atbol())
-
-    def test_addstr(self):
-        self.repl.complete = mock.Mock(True)
-
-        self.repl.s = "foo"
-        self.repl.addstr("bar")
-        self.assertEqual(self.repl.s, "foobar")
-
-        self.repl.cpos = 3
-        self.repl.addstr("buzz")
-        self.assertEqual(self.repl.s, "foobuzzbar")
-
-
-class TestCliReplTab(unittest.TestCase):
-    def setUp(self):
-        self.repl = FakeCliRepl()
-
-    # 3 Types of tab complete
-    def test_simple_tab_complete(self):
-        self.repl.matches_iter = MagicIterMock()
-        self.repl.matches_iter.__bool__.return_value = False
-        self.repl.complete = mock.Mock()
-        self.repl.print_line = mock.Mock()
-        self.repl.matches_iter.is_cseq.return_value = False
-        self.repl.show_list = mock.Mock()
-        self.repl.funcprops = mock.Mock()
-        self.repl.arg_pos = mock.Mock()
-        self.repl.matches_iter.cur_line.return_value = (None, "foobar")
-
-        self.repl.s = "foo"
-        self.repl.tab()
-        self.assertTrue(self.repl.complete.called)
-        self.repl.complete.assert_called_with(tab=True)
-        self.assertEqual(self.repl.s, "foobar")
-
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_substring_tab_complete(self):
-        self.repl.s = "bar"
-        self.repl.config.autocomplete_mode = (
-            autocomplete.AutocompleteModes.FUZZY
-        )
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "foobar")
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "foofoobar")
-
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_fuzzy_tab_complete(self):
-        self.repl.s = "br"
-        self.repl.config.autocomplete_mode = (
-            autocomplete.AutocompleteModes.FUZZY
-        )
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "foobar")
-
-    # Edge Cases
-    def test_normal_tab(self):
-        """make sure pressing the tab key will
-        still in some cases add a tab"""
-        self.repl.s = ""
-        self.repl.config = mock.Mock()
-        self.repl.config.tab_length = 4
-        self.repl.complete = mock.Mock()
-        self.repl.print_line = mock.Mock()
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "    ")
-
-    def test_back_parameter(self):
-        self.repl.matches_iter = mock.Mock()
-        self.repl.matches_iter.matches = True
-        self.repl.matches_iter.previous.return_value = "previtem"
-        self.repl.matches_iter.is_cseq.return_value = False
-        self.repl.show_list = mock.Mock()
-        self.repl.funcprops = mock.Mock()
-        self.repl.arg_pos = mock.Mock()
-        self.repl.matches_iter.cur_line.return_value = (None, "previtem")
-        self.repl.print_line = mock.Mock()
-        self.repl.s = "foo"
-        self.repl.cpos = 0
-        self.repl.tab(back=True)
-        self.assertTrue(self.repl.matches_iter.previous.called)
-        self.assertTrue(self.repl.s, "previtem")
-
-    # Attribute Tests
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_fuzzy_attribute_tab_complete(self):
-        """Test fuzzy attribute with no text"""
-        self.repl.s = "Foo."
-        self.repl.config.autocomplete_mode = (
-            autocomplete.AutocompleteModes.FUZZY
-        )
-
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "Foo.foobar")
-
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_fuzzy_attribute_tab_complete2(self):
-        """Test fuzzy attribute with some text"""
-        self.repl.s = "Foo.br"
-        self.repl.config.autocomplete_mode = (
-            autocomplete.AutocompleteModes.FUZZY
-        )
-
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "Foo.foobar")
-
-    # Expand Tests
-    def test_simple_expand(self):
-        self.repl.s = "f"
-        self.cpos = 0
-        self.repl.matches_iter = mock.Mock()
-        self.repl.matches_iter.is_cseq.return_value = True
-        self.repl.matches_iter.substitute_cseq.return_value = (3, "foo")
-        self.repl.print_line = mock.Mock()
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "foo")
-
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_substring_expand_forward(self):
-        self.repl.config.autocomplete_mode = (
-            autocomplete.AutocompleteModes.SUBSTRING
-        )
-        self.repl.s = "ba"
-        self.repl.tab()
-        self.assertEqual(self.repl.s, "bar")
-
-    @unittest.skip("disabled while non-simple completion is disabled")
-    def test_fuzzy_expand(self):
-        pass
 
 
 if __name__ == "__main__":
--- doc/sphinx/source/windows.rst
+++ doc/sphinx/source/windows.rst
@@ -7,9 +7,3 @@ other platforms as well.
 
 There are no official binaries for bpython on Windows (though this is something
 we plan on providing in the future).
-
-The easiest way to get `bpython.cli` (the curses frontend running) is to install
-an unofficial windows binary for pdcurses from:
-http://www.lfd.uci.edu/~gohlke/pythonlibs/#curses. After this you can just
-`pip install bpython` and run bpython curses frontend like you would on a Linux
-system (e.g. by typing `bpython-curses` on your prompt).
