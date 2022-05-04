2c80e03655c79527f05abb5a2e12a733bd50d11a
Sebastian Ramacher <sebastian@ramacher.at>
2022-05-05 00:52:30 +0200

    Add type annotations

--- bpython/curtsiesfrontend/preprocess.py
+++ bpython/curtsiesfrontend/preprocess.py
@@ -1,8 +1,11 @@
 """Tools for preparing code to be run in the REPL (removing blank lines,
 etc)"""
 
-from ..lazyre import LazyReCompile
+from codeop import CommandCompiler
+from typing import Match
 from itertools import tee, islice, chain
+
+from ..lazyre import LazyReCompile
 
 # TODO specifically catch IndentationErrors instead of any syntax errors
 
@@ -10,25 +13,26 @@ indent_empty_lines_re = LazyReCompile(r"\s*")
 tabs_to_spaces_re = LazyReCompile(r"^\t+")
 
 
-def indent_empty_lines(s, compiler):
+def indent_empty_lines(s: str, compiler: CommandCompiler) -> str:
     """Indents blank lines that would otherwise cause early compilation
 
     Only really works if starting on a new line"""
-    lines = s.split("\n")
+    initial_lines = s.split("\n")
     ends_with_newline = False
-    if lines and not lines[-1]:
+    if initial_lines and not initial_lines[-1]:
         ends_with_newline = True
-        lines.pop()
+        initial_lines.pop()
     result_lines = []
 
-    prevs, lines, nexts = tee(lines, 3)
+    prevs, lines, nexts = tee(initial_lines, 3)
     prevs = chain(("",), prevs)
     nexts = chain(islice(nexts, 1, None), ("",))
 
     for p_line, line, n_line in zip(prevs, lines, nexts):
         if len(line) == 0:
-            p_indent = indent_empty_lines_re.match(p_line).group()
-            n_indent = indent_empty_lines_re.match(n_line).group()
+            # "\s*" always matches
+            p_indent = indent_empty_lines_re.match(p_line).group()  # type: ignore
+            n_indent = indent_empty_lines_re.match(n_line).group()  # type: ignore
             result_lines.append(min([p_indent, n_indent], key=len) + line)
         else:
             result_lines.append(line)
@@ -36,17 +40,14 @@ def indent_empty_lines(s, compiler):
     return "\n".join(result_lines) + ("\n" if ends_with_newline else "")
 
 
-def leading_tabs_to_spaces(s):
-    lines = s.split("\n")
-    result_lines = []
-
-    def tab_to_space(m):
+def leading_tabs_to_spaces(s: str) -> str:
+    def tab_to_space(m: Match[str]) -> str:
         return len(m.group()) * 4 * " "
 
-    for line in lines:
-        result_lines.append(tabs_to_spaces_re.sub(tab_to_space, line))
-    return "\n".join(result_lines)
+    return "\n".join(
+        tabs_to_spaces_re.sub(tab_to_space, line) for line in s.split("\n")
+    )
 
 
-def preprocess(s, compiler):
+def preprocess(s: str, compiler: CommandCompiler) -> str:
     return indent_empty_lines(leading_tabs_to_spaces(s), compiler)
