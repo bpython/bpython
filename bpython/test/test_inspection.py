1332d18b53fbe99f2808febd035dbbb4647c02c9
Sebastian Ramacher <sebastian@ramacher.at>
2022-08-29 18:20:44 +0200

    Test inspection with static methods
    This is another variant of the class from #966.

--- bpython/test/test_inspection.py
+++ bpython/test/test_inspection.py
@@ -218,6 +218,43 @@ class TestInspection(unittest.TestCase):
         self.assertEqual(props.argspec.args, ["number", "lst"])
         self.assertEqual(props.argspec.defaults[0], [])
 
+    def test_issue_966_static_method(self):
+        class Issue966(Sequence):
+            @staticmethod
+            def cmethod(number: int, lst: List[int] = []):
+                """
+                Return a list of numbers
+
+                Example:
+                ========
+                C.cmethod(1337, [1, 2]) # => [1, 2, 1337]
+                """
+                return lst + [number]
+
+            @staticmethod
+            def bmethod(number, lst):
+                """
+                Return a list of numbers
+
+                Example:
+                ========
+                C.cmethod(1337, [1, 2]) # => [1, 2, 1337]
+                """
+                return lst + [number]
+
+        props = inspection.getfuncprops(
+            "bmethod", inspection.getattr_safe(Issue966, "bmethod")
+        )
+        self.assertEqual(props.func, "bmethod")
+        self.assertEqual(props.argspec.args, ["number", "lst"])
+
+        props = inspection.getfuncprops(
+            "cmethod", inspection.getattr_safe(Issue966, "cmethod")
+        )
+        self.assertEqual(props.func, "cmethod")
+        self.assertEqual(props.argspec.args, ["number", "lst"])
+        self.assertEqual(props.argspec.defaults[0], [])
+
 
 class A:
     a = "a"
