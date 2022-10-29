e20bf119949c445fbb1215f402d67057343476e3
Sebastian Ramacher <sebastian@ramacher.at>
2022-10-29 17:54:09 +0200

    Skip test with special unicode chars on broken Python 3.11 versions

--- bpython/test/test_curtsies_painting.py
+++ bpython/test/test_curtsies_painting.py
@@ -13,7 +13,7 @@ from curtsies.formatstringarray import (
 )
 from curtsies.fmtfuncs import cyan, bold, green, yellow, on_magenta, red
 from curtsies.window import CursorAwareWindow
-from unittest import mock
+from unittest import mock, skipIf
 
 from bpython.curtsiesfrontend.events import RefreshRequestEvent
 from bpython import config, inspection
@@ -311,6 +311,10 @@ class TestWidthAwareness(HigherLevelCurtsiesPaintingTest):
         cursor_pos = self.repl.paint()[1]
         self.assertEqual(cursor_pos, (1, 4))
 
+    @skipIf(
+        sys.version_info[:2] >= (3, 11) and sys.version_info[:3] < (3, 11, 1),
+        "https://github.com/python/cpython/issues/98744",
+    )
     def test_display_of_padding_chars(self):
         self.repl.width = 11
         [self.repl.add_normal_character(c) for c in "ｗｉｄｔｈ"]
